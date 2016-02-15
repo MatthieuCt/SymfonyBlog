5f6bef08671f6c6dfef8cea47a27d9b2f824e4dd
MatthieuCt <cayet.matthieu@gmail.com>
2016-02-15 22:58:08 +0100

    Missing integration (dsl c moch je c,je v eceyay 2 fR 1 truk bo)

--- src/BlogBundle/Listener/CookieListener.php
+++ src/BlogBundle/Listener/CookieListener.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace BlogBundle\Listener;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
@@ -8,10 +9,8 @@ class CookieListener
 {
     public function checkCookies(GetResponseEvent $event)
     {
-//        $cookies = $event->getRequest()->cookies->all();
-        $cookie = $event->getRequest()->cookies->get('cookie');
-        if (is_null($cookie)) {
-            $event->getRequest()->query->set('cookie', $cookie);
+        if (!isset($_COOKIE['cookie'])) {
+            setcookie('cookie', true);
         }
     }
 }
